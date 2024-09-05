0a191bf15ccb6a69e421db9abdc4f59c3b984643
majestymewtwo <majestymewtwo@gmail.com>
2024-09-05 20:39:12 +0530

    home page responsiveness

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -26,21 +26,21 @@ const CountdownTimer = ({ targetDate }) => {
   }, [timeLeft]);
 
   return (
-    <div>
+    <div className="space-y-7">
       <h1 className='text-white text-xl text-center font-bold drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
         starts in
       </h1>
       <div className='flex justify-center items-baseline w-2/3 mx-auto lg:w-full gap-1 lg:gap-4'>
         <TimeCard timeLeft={timeLeft.days} timeName='DAYS' />
-        <span className='text-7xl text-white drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
+        <span className='text-3xl lg:text-7xl text-white drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
           :
         </span>
         <TimeCard timeLeft={timeLeft.hours} timeName='HOURS' />
-        <span className='text-7xl text-white drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
+        <span className='text-3xl lg:text-7xl text-white drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
           :
         </span>
         <TimeCard timeLeft={timeLeft.minutes} timeName='MINUTES' />
-        <span className='text-7xl text-white drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
+        <span className='text-3xl lg:text-7xl text-white drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
           :
         </span>
         <TimeCard timeLeft={timeLeft.seconds} timeName='SECONDS' />
@@ -54,7 +54,7 @@ export default CountdownTimer;
 const TimeCard = ({ timeLeft, timeName }) => {
   return (
     <div className='flex flex-col justify-center items-center space-y-1 drop-shadow-[0_1px_1px_rgba(0,0,0,0.8)] rounded-lg text-white px-2'>
-      <h1 className='text-7xl font-sandy-toes-wave drop-shadow-[0_1px_1px_rgba(0,0,0,0.8)]'>
+      <h1 className='text-5xl lg:text-7xl font-sandy-toes-wave drop-shadow-[0_1px_1px_rgba(0,0,0,0.8)]'>
         {timeLeft}
       </h1>
       <h2 className='text-lg font-bold font-sandy-toes tracking-wide drop-shadow-[0_1px_1px_rgba(0,0,0,0.8)]'>
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -8,46 +8,48 @@ import NavButton from "@/components/NavButton";
 
 const Details = () => {
   return (
-    <div className='z-10 flex flex-col space-y-6 items-center p-6 lg:py-10 w-full lg:overflow-y-scroll'>
-      <div className='text-white text-center space-y-2'>
-        <h3 className='text-2xl lg:gap-2 flex flex-col lg:flex-row font-black drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
-          <span>Department of</span>
-          <span>M.Tech Computer Science & Engineering</span>
-        </h3>
-        <p className='text-xs font-bold lg:text-sm drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
-          in association with
-        </p>
-        <h3 className='text-3xl font-black drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
-          Sai Infoblitz
-        </h3>
-        <p className='text-xs font-bold lg:text-sm drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
-          proudly presents
-        </p>
+    <>
+      <div className='z-10 flex flex-col space-y-6 items-center py-6 px-12 lg:py-10 w-full lg:overflow-y-scroll'>
+        <div className='text-white text-center space-y-2'>
+          <h3 className='text-2xl lg:gap-2 flex flex-col lg:flex-row font-black drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
+            <span>Department of</span>
+            <span>M.Tech Computer Science & Engineering</span>
+          </h3>
+          <p className='text-xs font-bold lg:text-sm drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
+            in association with
+          </p>
+          <h3 className='text-3xl font-black drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
+            Sai Infoblitz
+          </h3>
+          <p className='text-xs font-bold lg:text-sm drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
+            proudly presents
+          </p>
+        </div>
+        <Link
+          to='/'
+          className='select-none px-12 lg:px-0 flex items-baseline space-x-4 text-white drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
+          <h1 className='font-lost-fish text-3xl lg:text-8xl'>Innovatia</h1>
+          <h1 className='font-sandy-toes-starfish text-5xl lg:text-9xl'>3.</h1>
+          <h1 className='font-sandy-toes-palm text-4xl lg:text-9xl'>0</h1>
+        </Link>
+        <CountdownTimer targetDate={"09-11-2024"} />
+        <div className='flex flex-col lg:flex-row relative items-center gap-4'>
+          <NavButton path='/about' title='About' />
+          <NavButton path='/events' title='Events' />
+          <NavButton path='/schedule' title='Schedule' />
+          <NavButton path='/team' title='Team' />
+        </div>
       </div>
-      <Link
-        to='/'
-        className='select-none px-4 lg:px-0 flex items-baseline space-x-4 text-white drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
-        <h1 className='font-lost-fish text-4xl lg:text-8xl'>Innovatia</h1>
-        <h1 className='font-sandy-toes-starfish text-6xl lg:text-9xl'>3.</h1>
-        <h1 className='font-sandy-toes-palm text-6xl lg:text-9xl'>0</h1>
-      </Link>
-      <CountdownTimer targetDate={"09-11-2024"} />
-      <div className='flex flex-col lg:flex-row relative items-center gap-4'>
-        <NavButton path='/about' title='About' />
-        <NavButton path='/events' title='Events' />
-        <NavButton path='/schedule' title='Schedule' />
-        <NavButton path='/team' title='Team' />
-      </div>
-      <div className='space-y-4 text-white text-center'>
+      <div className='space-y-4 text-white text-center w-full'>
         <h1 className='text-6xl font-beauty-mountain drop-shadow-[0_2.2px_2.2px_rgba(0,0,0,0.8)]'>
           Promo Video
         </h1>
         <video
-          className='lg:w-1/2 focus:outline-none mx-auto rounded-md'
+          className='w-[80%] lg:w-1/2 focus:outline-none mx-auto rounded-md'
           src='/promo.mp4'
           controls></video>
       </div>
-    </div>
+    </>
   );
 };
 
